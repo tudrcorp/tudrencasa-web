b46b58b8692f94766e3c64362c690567fd992ba8
Gustavo <administrador@sqlapio.com>
2025-06-13 20:23:21 -0400

    subiendo cambios importantes

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import {
+    Livewire,
+    Alpine,
+} from "../../vendor/livewire/livewire/dist/livewire.esm";
+import Clipboard from "@ryangjchandler/alpine-clipboard";
+
+Alpine.plugin(Clipboard);
+
+Livewire.start();
