45ee43a695f594f1d5bc2ee193fd8dcc23328bd8
Gustavo <administrador@sqlapio.com>
2025-06-13 20:40:58 -0400

    subiendo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
-import {
-    Livewire,
-    Alpine,
-} from "../../vendor/livewire/livewire/dist/livewire.esm";
-// import Clipboard from "@ryangjchandler/alpine-clipboard";
 
-// Alpine.plugin(Clipboard);
-
-Livewire.start();
